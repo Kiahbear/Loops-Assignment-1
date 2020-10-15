d3e814b78d4f5274d878a2a44ad822091e3dc40f
kiahb <kiahb@Kiahs-Laptop.niacc.edu>
2020-10-15 14:14:23 -0500

    added code

--- public_html/LoopsAssignment1.js
+++ public_html/LoopsAssignment1.js
@@ -3,5 +3,11 @@
  * 0, 10, 20, 30, ... 1000
  */
 
-document.write("This program displays the numbers 0 to 1000 by 10's");
+document.write("This program displays the numbers 0 to 1000 by 10's<br>");
 
+let counter = 0;
+while(counter <= 1000){
+    document.write(counter + "<br>");
+    counter = counter + 10;
+}
+document.write("Goodbye");
